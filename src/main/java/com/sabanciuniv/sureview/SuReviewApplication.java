fe555b849b65c0f03c7645f28f6d6ff6765edfa9
evesfect <evesfect@gmail.com>
2024-04-15 16:47:10 +0300

    SpringBoot init

---
+++ src/main/java/com/sabanciuniv/sureview/SuReviewApplication.java
@@ -0,0 +1,13 @@
+package com.sabanciuniv.sureview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabanciuniv/sureview/SuReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabanciuniv.sureview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
