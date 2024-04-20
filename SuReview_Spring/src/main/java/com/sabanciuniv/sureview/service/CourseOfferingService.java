3dd993fabd180221c1e9069955d79f564cf376f8
evesfect <evesfect@gmail.com>
2024-04-20 19:16:08 +0300

    CourseService and CourseOfferingService added

---
+++ SuReview_Spring/src/main/java/com/sabanciuniv/sureview/service/CourseOfferingService.java
@@ -0,0 +1,32 @@
+package com.sabanciuniv.sureview.service;
+
+import com.sabanciuniv.sureview.model.CourseOffering;
+import com.sabanciuniv.sureview.repository.CourseOfferingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CourseOfferingService implements GenericService<CourseOffering> {
+    private final CourseOfferingRepository courseOfferingRepository;
+
+    @Override
+    public CourseOffering save(CourseOffering courseOffering) {
+        return  courseOfferingRepository.save(courseOffering);
+    }
+
+    @Override
+    public CourseOffering findById(String id) {
+        return courseOfferingRepository.findById(id).orElseThrow(() -> new RuntimeException("Course Offering not found"));
+    }
+
+    @Override
+    public Iterable<CourseOffering> findAll() {
+        return courseOfferingRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(String id) {
+        courseOfferingRepository.deleteById(id);
+    }
+}
---
+++ SuReview_Spring/src/main/java/com/sabanciuniv/sureview/service/CourseService.java
@@ -0,0 +1,32 @@
+package com.sabanciuniv.sureview.service;
+
+import com.sabanciuniv.sureview.model.Course;
+import com.sabanciuniv.sureview.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CourseService implements GenericService<Course> {
+    private final CourseRepository courseRepository;
+
+    @Override
+    public Course save(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Course findById(String id) {
+        return courseRepository.findById(id).orElseThrow(() -> new RuntimeException("Course not found"));
+    }
+
+    @Override
+    public Iterable<Course> findAll() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(String id) {
+        courseRepository.deleteById(id);
+    }
+}
